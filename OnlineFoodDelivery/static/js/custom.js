48247d17c9672c56297e1795c1165e97cecda47b
Venkatesh Nagumantri <venkateshnagumantri01@gmail.com>
2023-05-03 11:21:07 +0530

    completed cart functionalities

--- OnlineFoodDelivery/static/js/custom.js
+++ OnlineFoodDelivery/static/js/custom.js
@@ -23,6 +23,12 @@ $(document).ready(function(){
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     $('#qty-'+food_id).html(response.qty);
+
+                    // subtotal, tax and grand total
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['grand_total']
+                    )
                 }
                 
                 
@@ -43,6 +49,7 @@ $(document).ready(function(){
         e.preventDefault();
         food_id = $(this).attr('data-id');
         url=$(this).attr('data-url');
+        cart_id = $(this).attr('id')
 
         $.ajax({
             type:'GET',
@@ -61,11 +68,84 @@ $(document).ready(function(){
                 else{
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     $('#qty-'+food_id).html(response.qty);
+
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['grand_total']
+                    )
+                    
+                    if(window.location.pathname == '/cart/')
+                    {
+                        removeCartItem(response.qty, cart_id);
+                        checkEmptyCart();
+                    }
+                    
                 }
                 
             }
         })
     })
+
+    // delete cart
+    $('.delete_cart').on('click', function(e){
+        e.preventDefault();
+        cart_id = $(this).attr('data-id');
+        url=$(this).attr('data-url');
+
+        $.ajax({
+            type:'GET',
+            url:url,
+            
+            success: function(response){
+                console.log(response);
+                if(response.status =='Failed'){
+                    swal(response.message, '', 'error');
+                }
+                else{
+                    $('#cart_counter').html(response.cart_counter['cart_count']);
+                    swal(response.status,response.message,'success');
+
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['grand_total']
+                    )
+
+                    removeCartItem(0, cart_id);
+                    checkEmptyCart();
+                }
+                
+            }
+        })
+    })
+
+    // delete the cart element if the qty is 0
+    function removeCartItem(cartItemQty, cart_id){
+        if(cartItemQty <= 0){
+            // remove the cart item element
+            document.getElementById("cart-item-"+cart_id).remove();
+        }
+    
+    }
+
+    // Check if the cart is empty
+    function checkEmptyCart(){
+        var cart_counter = document.getElementById('cart_counter').innerHTML
+        if(cart_counter == 0){
+            document.getElementById("empty-cart").style.display = "block";
+        }
+    }
+
+     // apply cart amounts
+     function applyCartAmounts(subtotal, grand_total)
+     {
+        if(window.location.pathname == '/cart/')
+        {
+            $('#subtotal').html(subtotal);
+            $('#total').html(grand_total);
+            
+
+        }
+     }
 
 });
 
